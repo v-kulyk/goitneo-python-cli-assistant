067211b3acbf5b0e817069eeab6112eafe4ebe33
Andy <andreyzp87@gmail.com>
2023-10-31 21:24:18 +0200

    run notes manager when necessary

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import sys
+
 from contacts.address_book_manager import AddressBookManager
 from contacts.address_book import AddressBook
 from contacts.user_interfaces import CommandLineInterface
 
+from notes.notes_manager import NotesManager
+from notes.notes_book import NotesBook
+from notes.user_interfaces import CommandLineInterface as NotesCommandLineInterface
+
+
 def main():
-    manager = AddressBookManager(address_book=AddressBook(), user_interface=CommandLineInterface())
+    if 'notes' in sys.argv:
+        manager = NotesManager(notes_book=NotesBook(), user_interface=NotesCommandLineInterface())
+    else:
+        manager = AddressBookManager(address_book=AddressBook(), user_interface=CommandLineInterface())
+
     manager.run()
+
 
 if __name__ == '__main__':
     main()
-
